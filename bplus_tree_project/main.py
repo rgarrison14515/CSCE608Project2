684ed5c4f14f12e90a84b1b07c1089e585acde83
rgarrison14515 <rgarrison14515@gmail.com>
2025-04-21 02:06:52 -0500

    Uncommented B+ test cases for video

--- bplus_tree_project/main.py
+++ bplus_tree_project/main.py
@@ -5,8 +5,7 @@ from bplustree import BPlusTree
 # File used for testing, getting output for project report
 if __name__ == "__main__":
     # test for dense tree
-    '''
-    # Generate a small but rich dataset (for visual clarity)
+    
     keys = generate_records(num_records=30)  # Fewer keys = more readable tree
 
     # Build a dense B+ tree of a smaller order for better printing
@@ -15,18 +14,18 @@ if __name__ == "__main__":
 
     print("Dense B+ Tree (order = 4, 30 keys):")
     tree.print_tree()
-    '''
+    
     #test for sparse tree
-    '''
+    
     keys = generate_records(num_records=30)
     order = 4
     tree = build_sparse_tree(keys, order)
     print("Sparse B+ Tree (order = 4, 30 keys, after deletions):")
     tree.print_tree()
-    '''
+    
 
     # insertion test
-    '''
+    
     # INSERTION DEMO: Force a split + promotion
     print("Insertion Demo (Order 3)")
     t = BPlusTree(order=3)
@@ -35,9 +34,9 @@ if __name__ == "__main__":
         print(f"\nInserting {k}:")
         t.insert(k)
         t.print_tree()
-    '''
+    
 
-    '''
+    
     # DELETION DEMO: Show merging + height drop
     print("\n Deletion Demo (Order 3)")
     t = BPlusTree(order=3)
@@ -52,4 +51,4 @@ if __name__ == "__main__":
         print(f"\nDeleting {k}:")
         t.delete(k)
         t.print_tree()
-    '''
+    
